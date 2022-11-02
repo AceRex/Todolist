e1a5860049d823532905710d49bc947bbf82323f
Are Oluwasegun <37227404+AceRex@users.noreply.github.com>
2022-11-02 13:14:00 +0100

    EJS

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const app = express();
 
 let newtodos = [];
-
+let workItems = [];
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -13,25 +13,40 @@ app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
   let today = new Date();
-  
+
   let options = {
     weekday: "long",
     day: "numeric",
-    month: "long"
-  }
+    month: "long",
+  };
   let day = today.toLocaleDateString("en-US", options);
 
-  res.render("list", { day: day, newtodo: newtodos});
+  res.render("list", { listTitle: day, newtodo: newtodos });
 });
 
-app.post("/", function (req, res){
+app.post("/", function (req, res) {
+
   newtodo = req.body.newtodo;
 
-  newtodos.push(newtodo)
+  if (req.body.list === "Work") {
+    workItems.push(newtodo);
+    res.redirect("/work");
+  } else {
+    newtodos.push(newtodo);
 
-  res.redirect("/");
+    res.redirect("/");
+  }
+});
 
-})
+app.get("/work", function (req, res) {
+  res.render("list", { listTitle: "Work List", newtodo: workItems });
+});
+
+app.post("/work", function (req, res) {
+  let newtodo = req.body.newtodo;
+  workItems.push(newtodo);
+  res.redirect("/work");
+});
 
 app.listen("3000", function () {
   console.log("Server is running correctly");
